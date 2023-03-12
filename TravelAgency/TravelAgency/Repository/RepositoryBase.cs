c0d97967bb0e2b75c8f3e3465ec85a8795efa1a8
Rippah <dzoni.pantic@hotmail.com>
2023-03-12 16:58:45 +0100

    Created repository base

---
+++ TravelAgency/TravelAgency/Repository/RepositoryBase.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Data.Sqlite;
+
+namespace TravelAgency.Repository
+{
+    public abstract class RepositoryBase
+    {
+        private readonly string _connectionString;
+        public RepositoryBase()
+        {
+            _connectionString = "Server=(locl); Database=MVVMLoginDb; Integrated Security = true";
+        }
+
+        protected SqliteConnection GetConnection()
+        {
+            return new SqliteConnection(_connectionString);
+        }
+    }
+}
