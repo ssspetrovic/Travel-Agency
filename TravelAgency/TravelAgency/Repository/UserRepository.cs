fc7cd07ba2af6cf290fbaee577f0610c65b9e9a3
Rippah <dzoni.pantic@hotmail.com>
2023-03-12 17:00:50 +0100

    Created user repository

---
+++ TravelAgency/TravelAgency/Repository/UserRepository.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using TravelAgency.Model;
+
+namespace TravelAgency.Repository
+{
+    public class UserRepository : RepositoryBase, IUserRepository
+    {
+        public bool AuthenticateUser(NetworkCredential credential)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Add(UserModel userModel)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Edit(UserModel userModel)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public UserModel GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public UserModel GetByUsername(string username)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<UserModel> GetByAll()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
