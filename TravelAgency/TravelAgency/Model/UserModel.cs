3b8f0eb9a584b853a4038952ee30626e4be3eaa9
Rippah <dzoni.pantic@hotmail.com>
2023-03-12 16:50:36 +0100

    Created user model

---
+++ TravelAgency/TravelAgency/Model/UserModel.cs
@@ -0,0 +1,30 @@
+﻿
+namespace TravelAgency.Model
+{
+    public class UserModel
+    {
+        public int Id { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public string Email { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+
+
+        public UserModel(int id, string userName, string password)
+        {
+            Id = id;
+            UserName = userName;
+            Password = password;
+        }
+
+        public UserModel(int id, string username, string password, string email, string name, string surname) : this(id, username, password)
+        {
+            Email = email;
+            Name = name;
+            Surname = surname;
+        }
+
+
+    }
+}
