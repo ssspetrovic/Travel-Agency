802af98a04c11095dcddc44d3bf139f1469414a5
Rippah <dzoni.pantic@hotmail.com>
2023-03-13 13:40:26 +0100

    Created front end for the guide

--- TravelAgency/TravelAgency/View/GuideView.xaml.cs
+++ TravelAgency/TravelAgency/View/GuideView.xaml.cs
@@ -1,27 +1,49 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Runtime.InteropServices;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
+using System.Windows.Interop;
 
 namespace TravelAgency.View
 {
     /// <summary>
     /// Interaction logic for GuideView.xaml
     /// </summary>
-    public partial class GuideView : Window
+    public partial class GuideView 
     {
         public GuideView()
         {
             InitializeComponent();
+            this.MaxHeight = SystemParameters.MaximizedPrimaryScreenHeight;
+        }
+
+        [DllImport("user32.dll")]
+        public static extern nint SendMessage(nint wnd,  int wMsg, nint wParam, nint lParam);
+
+        private void PanelControlBar_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            var helper = new WindowInteropHelper(this);
+            SendMessage(helper.Handle, 161, 2, 0);
+        }
+
+        private void PanelControlBar_MouseEnter(object sender, MouseEventArgs e)
+        {
+            this.MaxHeight = SystemParameters.MaximizedPrimaryScreenHeight;
+        }
+
+        private void Button_CloseClick(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void Button_MaximizeClick(object sender, RoutedEventArgs e)
+        {
+            this.WindowState = this.WindowState == WindowState.Normal ? WindowState.Maximized : WindowState.Normal;
+        }
+
+        private void Button_MinimizeClick(object sender, RoutedEventArgs e)
+        {
+            this.WindowState = WindowState.Minimized;
         }
     }
 }
