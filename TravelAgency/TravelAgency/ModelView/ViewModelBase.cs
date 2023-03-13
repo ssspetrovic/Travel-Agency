740aceb19cd6e6e9ff4dbfb2abb51a8b7b8f288b
Rippah <dzoni.pantic@hotmail.com>
2023-03-13 13:46:34 +0100

    Created Base ModelView

---
+++ TravelAgency/TravelAgency/ModelView/ViewModelBase.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+
+namespace TravelAgency.ModelView
+{
+    public abstract class ViewModelBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName))
+        }
+
+    }
+}
