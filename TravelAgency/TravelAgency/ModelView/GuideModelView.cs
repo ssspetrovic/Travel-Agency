cd23b3b826d67937de75cb799ebe8922e38b1aad
Rippah <dzoni.pantic@hotmail.com>
2023-03-13 14:10:58 +0100

    Created model view command

--- TravelAgency/TravelAgency/ModelView/GuideModelView.cs
+++ TravelAgency/TravelAgency/ModelView/GuideModelView.cs
@@ -1,7 +1,55 @@
 ﻿
+using System.Windows.Input;
+using TravelAgency.Model;
+using TravelAgency.Repository;
+
 namespace TravelAgency.ModelView
 {
     public class GuideModelView : BaseModelView
     {
+        private UserModel _currentUserAccount;
+        private BaseModelView _currentChildView;
+        private string _caption;
+
+        private IUserRepository userRepository;
+
+        public UserModel CurrentUserAccount
+        {
+            get { return _currentUserAccount; }
+            set
+            {
+                _currentUserAccount = value; 
+                OnPropertyChanged(nameof(CurrentUserAccount));
+            }
+        }
+
+        public BaseModelView CurrentChildView
+        {
+            get { return _currentChildView; }
+            set
+            {
+                _currentChildView = value;
+                OnPropertyChanged(nameof(CurrentChildView));
+            }
+        }
+
+        public string Caption
+        {
+            get { return _caption; }
+            set
+            {
+                _caption = value;
+                OnPropertyChanged(nameof(Caption));
+            }
+        }
+
+        public ICommand ShowGuideViewCommand { get; }
+        public ICommand ShowHomeViewCommand { get; }
+
+        public GuideModelView()
+        {
+            userRepository = new UserRepository();
+            CurrentUserAccount = new UserModel();
+        }
     }
 }
---
+++ TravelAgency/TravelAgency/ModelView/ModelViewCommand.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace TravelAgency.ModelView
+{
+    class ModelViewCommand : ICommand
+    {
+        private readonly Action<Object> _executeAction;
+        private readonly Action<Object> _canExecuteAction;
+
+        public bool CanExecute(object? parameter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Execute(object? parameter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public event EventHandler? CanExecuteChanged;
+    }
+}
