ac826160bb58c4368cc84bb57281d4487034d501
Srdjan Petrovic <s.petrovic852@gmail.com>
2023-03-13 01:33:20 +0100

    Small edit sign in, started sign up view

---
+++ TravelAgency/TravelAgency/View/SignUpView.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace TravelAgency.View
+{
+    /// <summary>
+    /// Interaction logic for SignUpView.xaml
+    /// </summary>
+    public partial class SignUpView : Window
+    {
+        public SignUpView()
+        {
+            InitializeComponent();
+        }
+        private void HeaderThumb_OnDragDelta(object sender, DragDeltaEventArgs e)
+        {
+            Left = Left + e.HorizontalChange;
+            Top = Top + e.VerticalChange;
+        }
+
+        private void CloseButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void SignUpButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
