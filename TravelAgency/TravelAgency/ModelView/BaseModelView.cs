4cd90e3bd9701305dcead8398f0ca3b4eb9df69a
Srdjan Petrovic <s.petrovic852@gmail.com>
2023-03-14 14:20:58 +0100

    Add CallerMemberName attribute to onpropertychanged in basemodelview

--- TravelAgency/TravelAgency/ModelView/BaseModelView.cs
+++ TravelAgency/TravelAgency/ModelView/BaseModelView.cs
@@ -1,4 +1,6 @@
-﻿using System.ComponentModel;
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
 
 namespace TravelAgency.ModelView
 {
@@ -6,10 +8,9 @@ namespace TravelAgency.ModelView
     {
         public event PropertyChangedEventHandler? PropertyChanged;
 
-        public void OnPropertyChanged(string propertyName)
+        protected virtual void OnPropertyChanged([CallerMemberName] string? propertyName = null)
         {
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
-
     }
 }
