529f328b1c1efc1bed70eeccfd225b4d2a03c6dc
Rippah <dzoni.pantic@hotmail.com>
2023-03-14 11:12:17 +0100

    Created location repository

---
+++ TravelAgency/TravelAgency/Model/ILocationRepository.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace TravelAgency.Model
+{
+    internal interface ILocationRepository
+    {
+        void Add(LocationModel locationModel);
+        void Edit(LocationModel locationModel);
+        void Remove(int id);
+        UserModel GetById(int id);
+        LocationModel GetByCity(string city);
+        IEnumerable<UserModel> GetByAll();
+    }
+}
---
+++ TravelAgency/TravelAgency/Repository/LocationRepository.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TravelAgency.Model;
+
+namespace TravelAgency.Repository
+{
+    public class LocationRepository : RepositoryBase, ILocationRepository
+    {
+        public void Add(LocationModel locationModel)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Edit(LocationModel locationModel)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public UserModel GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public LocationModel GetByCity(string city)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<UserModel> GetByAll()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
