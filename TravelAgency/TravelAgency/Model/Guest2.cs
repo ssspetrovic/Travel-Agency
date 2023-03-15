e3e146f4d44e91602fd71e952755b807e8bdd54a
Srdjan Petrovic <s.petrovic852@gmail.com>
2023-03-15 01:32:32 +0100

    Testing guest2 view

---
+++ TravelAgency/TravelAgency/Model/Guest2.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TravelAgency.Model
+{
+    internal class Guest2 : UserModel
+    {
+        private List<TourModel> _tours;
+
+        public Guest2()
+        {
+            _tours = new List<TourModel>();
+        }
+    }
+}
