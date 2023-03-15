de07ddf73e9792f3cbdf46bacd427b9b18a47b47
Srdjan Petrovic <s.petrovic852@gmail.com>
2023-03-15 12:36:01 +0100

    Implement command interface in base command

--- TravelAgency/TravelAgency/Command/BaseCommand.cs
+++ TravelAgency/TravelAgency/Command/BaseCommand.cs
@@ -3,10 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace TravelAgency.Command
 {
-    internal class BaseCommand
+    internal class BaseCommand : ICommand
     {
+        public bool CanExecute(object? parameter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Execute(object? parameter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public event EventHandler? CanExecuteChanged;
     }
 }
