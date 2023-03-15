1829a4b8adcd76e0cdb6b0dec6f03acfc7632a4a
Srdjan Petrovic <s.petrovic852@gmail.com>
2023-03-15 12:34:51 +0100

    Move commands from Controller folder to Command folder

--- TravelAgency/TravelAgency/Command/MakeTourReservationCommand.cs
+++ TravelAgency/TravelAgency/Command/MakeTourReservationCommand.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace TravelAgency.Controller
+namespace TravelAgency.Command
 {
-    internal class BaseCommand
+    internal class MakeTourReservationCommand
     {
     }
 }
