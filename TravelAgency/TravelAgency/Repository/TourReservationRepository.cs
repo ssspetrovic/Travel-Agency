0bbc82377f4703261f19a8bba41783c2343641dd
Srdjan Petrovic <s.petrovic852@gmail.com>
2023-03-20 14:07:15 +0100

    Add tour reservation repository class

---
+++ TravelAgency/TravelAgency/Repository/TourReservationRepository.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TravelAgency.Model;
+
+namespace TravelAgency.Repository
+{
+    internal class TourReservationRepository : RepositoryBase, ITourReservationRepository
+    {
+        public void Add(ActiveTourModel activeTourModel)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove()
+        {
+            throw new NotImplementedException();
+        }
+
+        public ActiveTourModel GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
