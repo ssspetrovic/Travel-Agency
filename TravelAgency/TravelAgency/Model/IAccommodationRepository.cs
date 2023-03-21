a49e98f21dd7d092b2bed14ee2006f9d2dae776f
agajic <andrejgajic777@gmail.com>
2023-03-21 01:33:14 +0100

    Added Accommodation Repository

---
+++ TravelAgency/TravelAgency/Model/IAccommodationRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TravelAgency.Model
+{
+    internal interface IAccommodationRepository
+    {
+        void Add(Accommodation accommodation);
+    }
+}
---
+++ TravelAgency/TravelAgency/Repository/AccommodationRepository.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Data.Sqlite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TravelAgency.Model;
+
+namespace TravelAgency.Repository
+{
+    internal class AccommodationRepository : RepositoryBase, IAccommodationRepository
+    {
+        public void Add(Accommodation accommodation)
+        {
+            using var databaseConnection = GetConnection();
+            databaseConnection.Open();
+
+            const string insertStatement =
+                @"insert into Accommodation(name, type, maxGuests, minGuests, locationId, adress, reservableDays, images, description) 
+                    values ($name, $type, $maxGuests, $minGuests, $locationId, $adress, $reservableDays, $images, $description)";
+            using var insertCommand = new SqliteCommand(insertStatement, databaseConnection);
+            insertCommand.Parameters.AddWithValue("$name", accommodation.Name);
+            insertCommand.Parameters.AddWithValue("$type", accommodation.Type);
+            insertCommand.Parameters.AddWithValue("$maxGuests", accommodation.MaxReservationDays);
+            insertCommand.Parameters.AddWithValue("$minGuests", accommodation.MinReservationDays);
+            insertCommand.Parameters.AddWithValue("$locationId", accommodation.LocationId);
+            insertCommand.Parameters.AddWithValue("$adress", accommodation.Adress);
+            insertCommand.Parameters.AddWithValue("$reservableDays", accommodation.ReservableDays);
+            insertCommand.Parameters.AddWithValue("$images", accommodation.Images);
+            insertCommand.Parameters.AddWithValue("$description", accommodation.Description);
+            insertCommand.ExecuteNonQuery();
+        }
+    }
+}
