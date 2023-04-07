db5b3c65df82817bbaa8e9a598e2a5dd162230d2
Srdjan Petrovic <s.petrovic852@gmail.com>
2023-04-07 21:17:32 +0200

    Create voucher repository

---
+++ TravelAgency/TravelAgency/Repository/TourVoucherRepository.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TravelAgency.Model;
+
+namespace TravelAgency.Repository
+{
+    internal class TourVoucherRepository : ITourVoucherRepository
+    {
+        public void Add(TourVoucher tourVoucher)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteExpired()
+        {
+            throw new NotImplementedException();
+        }
+
+        public ObservableCollection<TourVoucher> GetAllAsCollection()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
