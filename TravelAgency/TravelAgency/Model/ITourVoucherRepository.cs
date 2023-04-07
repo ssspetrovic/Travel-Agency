1a6cc56f284b869e82ad6c4c62194ea2e609fef7
Srdjan Petrovic <s.petrovic852@gmail.com>
2023-04-07 21:16:36 +0200

    Complete voucher repository interface

---
+++ TravelAgency/TravelAgency/Model/ITourVoucherRepository.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.ObjectModel;
+
+namespace TravelAgency.Model
+{
+    internal interface ITourVoucherRepository
+    {
+        void Add(TourVoucher tourVoucher);
+        void DeleteById(int id);
+        void DeleteExpired();
+        ObservableCollection<TourVoucher> GetAllAsCollection();
+    }
+}
