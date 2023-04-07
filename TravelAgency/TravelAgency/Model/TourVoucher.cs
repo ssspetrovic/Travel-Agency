f761bd313e64aec55b1fdae15883dec5c1ac3055
Srdjan Petrovic <s.petrovic852@gmail.com>
2023-04-07 21:09:23 +0200

    Complete voucher model

---
+++ TravelAgency/TravelAgency/Model/TourVoucher.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TravelAgency.Model
+{
+    internal class TourVoucher
+    {
+        public int Id { get; set; }
+        public string Description { get; set; }
+        public string Date { get; set; }
+
+        public TourVoucher(int id, string description, string date)
+        {
+            Id = id;
+            Description = description;
+            Date = date;
+        }
+    }
+}
