e941d913fae258dd6f8ae378660d7d9725daed34
Srdjan Petrovic <s.petrovic852@gmail.com>
2023-04-11 15:50:43 +0200

    Add rate tour view

---
+++ TravelAgency/TravelAgency/View/Controls/Tourist/RateTourView.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace TravelAgency.View.Controls.Tourist
+{
+    /// <summary>
+    /// Interaction logic for RateTourView.xaml
+    /// </summary>
+    public partial class RateTourView : Window
+    {
+        public RateTourView()
+        {
+            InitializeComponent();
+        }
+
+        private void SignOutButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            var signInView = new SignInView();
+            signInView.Show();
+            Close();
+        }
+
+        private void HeaderThumb_OnDragDelta(object sender, DragDeltaEventArgs e)
+        {
+            Left += e.HorizontalChange;
+            Top += e.VerticalChange;
+        }
+
+        private void CloseButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void TourReservationButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            var tourReservation = new TourReservationView();
+            tourReservation.Show();
+            Close();
+        }
+        private void MyToursButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            var myToursView = new MyToursView();
+            myToursView.Show();
+            Close();
+        }
+        private void MyVouchersButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            var myVouchersView = new TourVouchers();
+            myVouchersView.Show();
+            Close();
+        }
+    }
+}
