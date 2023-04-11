00ab535d72e3e0785f987a40f46077624f167008
Srdjan Petrovic <s.petrovic852@gmail.com>
2023-04-11 15:54:53 +0200

    Add rate tour view model

--- TravelAgency/TravelAgency/View/Controls/Tourist/RateTourView.xaml.cs
+++ TravelAgency/TravelAgency/View/Controls/Tourist/RateTourView.xaml.cs
@@ -1,17 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
+﻿using System.Windows;
 using System.Windows.Controls.Primitives;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
+using TravelAgency.ViewModel;
 
 namespace TravelAgency.View.Controls.Tourist
 {
@@ -23,6 +12,7 @@ namespace TravelAgency.View.Controls.Tourist
         public RateTourView()
         {
             InitializeComponent();
+            DataContext = new RateTourViewModel();
         }
 
         private void SignOutButton_OnClick(object sender, RoutedEventArgs e)
@@ -49,6 +39,7 @@ namespace TravelAgency.View.Controls.Tourist
             tourReservation.Show();
             Close();
         }
+
         private void MyToursButton_OnClick(object sender, RoutedEventArgs e)
         {
             var myToursView = new MyToursView();
