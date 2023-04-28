003c363628a0d9d8ee257d0adc575389d37b4309
Srdjan Petrovic <s.petrovic852@gmail.com>
2023-04-28 19:55:13 +0200

    Create regular tour request view

---
+++ TravelAgency/TravelAgency/View/Controls/Tourist/RegularTourRequestView.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TravelAgency.View.Controls.Tourist
+{
+    /// <summary>
+    /// Interaction logic for RegularTourRequestView.xaml
+    /// </summary>
+    public partial class RegularTourRequestView : Page
+    {
+        public RegularTourRequestView()
+        {
+            InitializeComponent();
+        }
+    }
+}
