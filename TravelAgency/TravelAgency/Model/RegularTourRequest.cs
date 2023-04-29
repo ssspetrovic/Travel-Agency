32104a9814b5d580bfb938b597b96e9c136b2d80
Srdjan Petrovic <s.petrovic852@gmail.com>
2023-04-29 13:12:22 +0200

    Complete model

---
+++ TravelAgency/TravelAgency/Model/RegularTourRequest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace TravelAgency.Model
+{
+    internal class RegularTourRequest
+    {
+        public Location Location { get; set; }
+        public Language Language { get; set; }
+        public DateTime StartingDate { get; set; }
+        public DateTime EndingDate { get; set; }
+        public string Description { get; set; }
+
+        public RegularTourRequest(Location location, Language language, DateTime startingDate, DateTime endingDate, string description)
+        {
+            Location = location;
+            Language = language;
+            StartingDate = startingDate;
+            EndingDate = endingDate;
+            Description = description;
+        }
+    }
+}
