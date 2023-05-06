61e6a5a2eef0986407e103ef0e60995d8e12798d
Srdjan Petrovic <s.petrovic852@gmail.com>
2023-05-06 22:10:56 +0200

    Add BaseCommand

---
+++ TravelAgency/TravelAgency/Command/BaseCommand.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace TravelAgency.Command
+{
+    public class BaseCommand<T> : ICommand
+    {
+        private readonly Action<T> _execute;
+        private readonly Predicate<T>? _canExecute;
+
+        public BaseCommand(Action<T> execute) : this(execute, null)
+        {
+            _execute = execute;
+        }
+
+        public BaseCommand(Action<T> execute, Predicate<T>? canExecute)
+        {
+            _execute = execute;
+            _canExecute = canExecute;
+        }
+
+        public bool CanExecute(object? parameter)
+        {
+            if (_canExecute is not null && parameter is T tParameter)
+            {
+                return _canExecute(tParameter);
+            }
+
+            return _execute != null;
+        }
+
+        public void Execute(object? parameter)
+        {
+            if (parameter is T tParameter)
+                _execute?.Invoke(tParameter);
+        }
+
+        public event EventHandler? CanExecuteChanged = delegate { };
+    }
+}
