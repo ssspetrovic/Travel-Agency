8c7e6c1ae46fb9c2a9cb1f7d4302ed5f8dda68e7
Srdjan Petrovic <s.petrovic852@gmail.com>
2023-05-07 13:09:34 +0200

    Initialize commands in view model ctor

--- TravelAgency/TravelAgency/ViewModel/TouristViewModel.cs
+++ TravelAgency/TravelAgency/ViewModel/TouristViewModel.cs
@@ -9,18 +9,16 @@ namespace TravelAgency.ViewModel
     public class TouristViewModel
     {
         #region Fields and Properties
-        public NavigationService NavigationService { get; set; }
+        private NavigationService NavigationService { get; set; }
         public RelayCommand NavigateToHomePageCommand { get; set; }
         public RelayCommand NavigateToMyToursPageCommand { get; set; }
-        //public RelayCommand NavigateToMyTourVouchersPageCommand { get; set; }
-        //public RelayCommand NavigateToRateTourPageCommand { get; set; }
-        //public RelayCommand NavigateTRegularTourRequestPageCommand { get; set; }
-        //public RelayCommand NavigateToRequestTourPageCommand { get; set; }
-        //public RelayCommand NavigateToTourReservationPageCommand { get; set; }
+        public RelayCommand NavigateToMyTourVouchersPageCommand { get; set; }
+        public RelayCommand NavigateToRateTourPageCommand { get; set; }
+        public RelayCommand NavigateToRequestTourPageCommand { get; set; }
+        public RelayCommand NavigateToTourReservationPageCommand { get; set; }
         #endregion
 
         #region Actions
-
         private bool CanExecute_NavigationCommand(object? parameter)
         {
             return true;
@@ -28,7 +26,6 @@ namespace TravelAgency.ViewModel
 
         private void Execute_NavigateToHomePageCommand(object? parameter)
         {
-            Debug.WriteLine("In");
             NavigationService.Navigate(new HomeView());
         }
 
@@ -37,17 +34,42 @@ namespace TravelAgency.ViewModel
             NavigationService.Navigate(new MyToursView());
         }
 
+        private void Execute_NavigateToRateTourPageCommand(object? parameter)
+        {
+            NavigationService.Navigate(new HomeView());
+        }
+
+        private void Execute_NavigateToMyTourVouchersPageCommand(object? parameter)
+        {
+            NavigationService.Navigate(new HomeView());
+        }
+
+        private void Execute_NavigateToRequestTourPageCommand(object? parameter)
+        {
+            NavigationService.Navigate(new HomeView());
+        }
+
+        private void Execute_NavigateToTourReservationPageCommand(object? parameter)
+        {
+            NavigationService.Navigate(new HomeView());
+        }
         #endregion
 
         public TouristViewModel(NavigationService navigationService)
         {
-            Debug.WriteLine("In vm");
             NavigationService = navigationService;
-            Debug.WriteLine(NavigateToHomePageCommand);
             NavigateToHomePageCommand =
                 new RelayCommand(Execute_NavigateToHomePageCommand, CanExecute_NavigationCommand);
             NavigateToMyToursPageCommand =
                 new RelayCommand(Execute_NavigateToMyToursPageCommand, CanExecute_NavigationCommand);
+            NavigateToRateTourPageCommand =
+                new RelayCommand(Execute_NavigateToRateTourPageCommand, CanExecute_NavigationCommand);
+            NavigateToMyTourVouchersPageCommand =
+                new RelayCommand(Execute_NavigateToMyTourVouchersPageCommand, CanExecute_NavigationCommand);
+            NavigateToRequestTourPageCommand =
+                new RelayCommand(Execute_NavigateToRequestTourPageCommand, CanExecute_NavigationCommand);
+            NavigateToTourReservationPageCommand =
+                new RelayCommand(Execute_NavigateToTourReservationPageCommand, CanExecute_NavigationCommand);
         }
     }
 }
