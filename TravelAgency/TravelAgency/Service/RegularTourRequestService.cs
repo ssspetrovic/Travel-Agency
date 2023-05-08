da9f37671f4c3a0ab89c97f5cb83bfc4204abee6
Srdjan Petrovic <s.petrovic852@gmail.com>
2023-05-08 15:17:51 +0200

    Add service class

---
+++ TravelAgency/TravelAgency/Service/RegularTourRequestService.cs
@@ -0,0 +1,20 @@
+﻿using TravelAgency.Model;
+using TravelAgency.Repository;
+
+namespace TravelAgency.Service
+{
+    internal class RegularTourRequestService
+    {
+        private readonly RegularTourRequestRepository _tourRequestRepository;
+
+        public void Add(RegularTourRequest tourRequest)
+        {
+            _tourRequestRepository.Add(tourRequest);
+        }
+
+        public RegularTourRequestService()
+        {
+            _tourRequestRepository = new RegularTourRequestRepository();
+        }
+    }
+}
