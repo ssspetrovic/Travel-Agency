d2dd974e7ad7a861faa9018e0bd75cccf1273f55
Srdjan Petrovic <s.petrovic852@gmail.com>
2023-05-10 17:30:41 +0200

    Create new view as page

---
+++ TravelAgency/TravelAgency/View/Tourist/UserProfileView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace TravelAgency.View.Tourist
+{
+    /// <summary>
+    /// Interaction logic for UserProfileView.xaml
+    /// </summary>
+    public partial class UserProfileView : Page
+    {
+        public UserProfileView()
+        {
+            InitializeComponent();
+        }
+    }
+}
