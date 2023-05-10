4085d0dd755dce5578d9aa7c003a6ea2059c0c43
Srdjan Petrovic <s.petrovic852@gmail.com>
2023-05-10 23:35:11 +0200

    Remove drag delta handler

--- TravelAgency/TravelAgency/View/Tourist/TouristView.xaml.cs
+++ TravelAgency/TravelAgency/View/Tourist/TouristView.xaml.cs
@@ -14,11 +14,5 @@ namespace TravelAgency.View.Tourist
             HomeButton.IsChecked = true;
             DataContext = new TouristViewModel(ContentFrame.NavigationService);
         }
-
-        private void HeaderThumb_OnDragDelta(object sender, DragDeltaEventArgs e)
-        {
-            Left += e.HorizontalChange;
-            Top += e.VerticalChange;
-        }
     }
 }
