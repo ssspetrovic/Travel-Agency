55e28f743291ff9a9bc860c63fb5d580d83b7bc3
Srdjan Petrovic <s.petrovic852@gmail.com>
2023-05-11 16:05:29 +0200

    Add tooltip label user control

---
+++ TravelAgency/TravelAgency/Resources/Controls/ToggleSwitchLabel.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace TravelAgency.Resources.Controls
+{
+    /// <summary>
+    /// Interaction logic for ToggleSwitchLabel.xaml
+    /// </summary>
+    public partial class ToggleSwitchLabel : UserControl
+    {
+        public ToggleSwitchLabel()
+        {
+            InitializeComponent();
+        }
+    }
+}
