8a2294fb6469a12056bcde17b0b32e47d3dec4e8
Srdjan Petrovic <s.petrovic852@gmail.com>
2023-05-12 17:50:29 +0200

    Create view class

---
+++ TravelAgency/TravelAgency/View/Tourist/MyRequestsView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace TravelAgency.View.Tourist
+{
+    /// <summary>
+    /// Interaction logic for MyRequestsView.xaml
+    /// </summary>
+    public partial class MyRequestsView : Page
+    {
+        public MyRequestsView()
+        {
+            InitializeComponent();
+        }
+    }
+}
