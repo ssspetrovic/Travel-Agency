3a5596c01155f5bd778ef9f7c752cc60e8c0eed5
Srdjan Petrovic <s.petrovic852@gmail.com>
2023-05-12 18:42:49 +0200

    Add main window corner radius, Connect view w/ view model and implement nav command

--- TravelAgency/TravelAgency/View/Tourist/MyRequestsView.xaml.cs
+++ TravelAgency/TravelAgency/View/Tourist/MyRequestsView.xaml.cs
@@ -1,13 +1,17 @@
-﻿namespace TravelAgency.View.Tourist
+﻿using System.Windows.Navigation;
+using TravelAgency.ViewModel.Tourist;
+
+namespace TravelAgency.View.Tourist
 {
     /// <summary>
     /// Interaction logic for MyRequestsView.xaml
     /// </summary>
     public partial class MyRequestsView
     {
-        public MyRequestsView()
+        public MyRequestsView(NavigationService navigationService, TouristViewModel touristViewModel)
         {
             InitializeComponent();
+            DataContext = new MyRequestsViewModel(navigationService, touristViewModel);
         }
     }
 }
--- TravelAgency/TravelAgency/ViewModel/Tourist/MyRequestsViewModel.cs
+++ TravelAgency/TravelAgency/ViewModel/Tourist/MyRequestsViewModel.cs
@@ -1,7 +1,32 @@
-﻿namespace TravelAgency.ViewModel.Tourist
+﻿using System.Collections.ObjectModel;
+using System.Windows.Navigation;
+using TravelAgency.Model;
+using TravelAgency.Service;
+
+namespace TravelAgency.ViewModel.Tourist
 {
     internal class MyRequestsViewModel : BaseViewModel
     {
-        //private readonly TourReq
+        private readonly NavigationService _navigationService;
+        private readonly TouristViewModel _touristViewModel;
+        private ObservableCollection<RegularTourRequest> _regularRequests;
+
+        public ObservableCollection<RegularTourRequest> RegularRequests
+        {
+            get => _regularRequests;
+            set
+            {
+                _regularRequests = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public MyRequestsViewModel(NavigationService navigationService, TouristViewModel touristViewModel)
+        {
+            var tourRequestService = new TourRequestService();
+            _navigationService = navigationService;
+            _touristViewModel = touristViewModel;
+            _regularRequests = tourRequestService.GetAllAsCollection();
+        }
     }
 }
