8064bd98cc7aa0ec315d7ce907a1c2b32cb48ca7
Srdjan Petrovic <s.petrovic852@gmail.com>
2023-05-13 11:04:58 +0200

    Create view

---
+++ TravelAgency/TravelAgency/View/Tourist/RequestsStatisticsView.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace TravelAgency.View.Tourist
+{
+    /// <summary>
+    /// Interaction logic for RequestsStatisticsView.xaml
+    /// </summary>
+    public partial class RequestsStatisticsView
+    {
+        public RequestsStatisticsView()
+        {
+            InitializeComponent();
+        }
+    }
+}
