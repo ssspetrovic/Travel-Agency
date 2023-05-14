786f7ea69563a0caf21866809848ee2cf547c12d
agajic <andrejgajic777@gmail.com>
2023-05-14 15:45:55 +0200

    small update

--- TravelAgency/TravelAgency/View/Controls/Owner/ScheduleRenovationView.xaml.cs
+++ TravelAgency/TravelAgency/View/Controls/Owner/ScheduleRenovationView.xaml.cs
@@ -29,6 +29,7 @@ namespace TravelAgency.View.Controls.Owner
         private readonly ScheduleRenovationViewModel _viewModel = new();
         AccommodationRepository accommodationRepository = new AccommodationRepository();
         ReservationService reservationService = new ReservationService();
+        RenovationRepository renovationRepository = new RenovationRepository();
         public ScheduleRenovationView()
         {
             InitializeComponent();
@@ -37,25 +38,77 @@ namespace TravelAgency.View.Controls.Owner
 
         private void btnCheck_Click(object sender, RoutedEventArgs e)
         {
-            this.FreeDatesListView.Items.Clear();
-
-            AccommodationDTO acc = accommodationRepository.GetByName(cmbAccName.Text);
-            DateTime startDate = (DateTime)pickStartDate.SelectedDate;
-            DateTime endDate = (DateTime)pickEndDate.SelectedDate;
-            int duration = Convert.ToInt32(txtDuration.Text);
-
-            List<FreeDatesDTO> freeDates = reservationService.GetFreeDates(acc.Id, duration, startDate, endDate);
-
-            string s = "";
-            int countFreeDates = 0;
-            foreach (FreeDatesDTO fd in freeDates)
+            if (pickStartDate.SelectedDate != null || pickEndDate.SelectedDate != null)
             {
-                s += fd.startDate.ToShortDateString() + " " + fd.endDate.ToShortDateString() + "\n";
-                this.FreeDatesListView.Items.Add(fd);
-                countFreeDates++;
+                this.FreeDatesListView.Items.Clear();
+
+                AccommodationDTO acc = accommodationRepository.GetByName(cmbAccName.Text);
+                DateTime startDate = (DateTime)pickStartDate.SelectedDate;
+                DateTime endDate = (DateTime)pickEndDate.SelectedDate;
+                int duration = Convert.ToInt32(txtDuration.Text);
+
+                List<FreeDatesDTO> freeDates = reservationService.GetFreeDates(acc.Id, duration, startDate, endDate);
+
+                string s = "";
+                int countFreeDates = 0;
+                foreach (FreeDatesDTO fd in freeDates)
+                {
+                    s += fd.startDate.ToShortDateString() + " " + fd.endDate.ToShortDateString() + "\n";
+                    this.FreeDatesListView.Items.Add(fd);
+                    countFreeDates++;
+                }
+                lblCountFreeDates.Content = "Free: " + countFreeDates.ToString();
+                MessageBox.Show(s);
             }
-            lblCountFreeDates.Content = "Free: " + countFreeDates.ToString();
-            MessageBox.Show(s);
+            else
+            {
+                MessageBox.Show("You need to select start and end date...", "Message");
+            }
+        }
+
+        private void btnAppoint_Click(object sender, RoutedEventArgs e)
+        {
+            if (lblStartDate.Content != null)
+            {
+                AccommodationDTO acc = accommodationRepository.GetByName(cmbAccName.Text);
+                DateTime startDate = Convert.ToDateTime(lblStartDate.Content);
+                DateTime endDate = Convert.ToDateTime(lblEndDate.Content);
+                int duration = Convert.ToInt32(txtDuration.Text);
+                string description = txtDescription.Text;
+
+                Renovation renovation = new Renovation(acc.Id, startDate, endDate, duration, description);
+                renovationRepository.Add(renovation);
+
+                if (CurrentLanguageAndTheme.languageId == 0)
+                    MessageBox.Show("Successfull appointed renovation!", "Message");
+                else
+                    MessageBox.Show("Uspešno zakazano renoviranje!", "Poruka");
+                Refresh();
+            }
+            else
+            {
+                if (CurrentLanguageAndTheme.languageId == 0)
+                    MessageBox.Show("You need to select a date first...", "Message");
+                else
+                    MessageBox.Show("Morate prvo datum izabrati...", "Poruka");
+            }
+        }
+
+        private void Refresh()
+        {
+            OwnerMainView mainWindow = null;
+            foreach (Window window in Application.Current.Windows)
+            {
+                if (window is OwnerMainView)
+                {
+                    mainWindow = (OwnerMainView)window;
+                    break;
+                }
+            }
+
+            Frame mainFrame = mainWindow.mainFrame;
+            ScheduleRenovationView scheduleRenovationView = new ScheduleRenovationView();
+            mainFrame.Navigate(scheduleRenovationView);
         }
     }
 }
