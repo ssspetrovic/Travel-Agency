42121e210acbca6800e13399a0e86c11a921c571
agajic <andrejgajic777@gmail.com>
2023-05-15 01:55:41 +0200

    created Accommodatio Stat

--- TravelAgency/TravelAgency/Service/AccommodationService.cs
+++ TravelAgency/TravelAgency/Service/AccommodationService.cs
@@ -13,10 +13,12 @@ namespace TravelAgency.Service
     public class AccommodationService : RepositoryBase
     {
         private readonly AccommodationRepository accommodationRepository;
+        private readonly ReservationService reservationService;
 
         public AccommodationService()
         {
             accommodationRepository = new AccommodationRepository();
+            reservationService = new ReservationService();
         }
 
         public ObservableCollection<string> GetAccommodationNames()
@@ -28,6 +30,24 @@ namespace TravelAgency.Service
                 names.Add(a.Name);
             }
             return names;
+        }
+
+        public ObservableCollection<AccommodationStatDTO> GetAccommodationStatByYear(int accID)
+        {
+            ObservableCollection<AccommodationStatDTO> accList = new ObservableCollection<AccommodationStatDTO>();
+
+            int yearStart = 2020;
+            AccommodationStatDTO statDTO = new AccommodationStatDTO();
+            for (int i = yearStart; i <= 2023; i++) 
+            {
+                statDTO.Period = i.ToString();
+                statDTO.ReservationCount = reservationService.GetReservationCountByYear(i, accID);
+
+
+                accList.Add(statDTO);
+            }
+
+            return accList;
         }
     }
 }
---
+++ TravelAgency/TravelAgency/ViewModel/AccommodationStatisticsViewModel.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using TravelAgency.DTO;
+using TravelAgency.Service;
+
+namespace TravelAgency.ViewModel
+{
+    public class AccommodationStatisticsViewModel : BaseViewModel
+    {
+        private readonly CollectionViewSource _accommodationsCollection;
+        public new event PropertyChangedEventHandler? PropertyChanged;
+
+        //private FreeDatesDTO? _selectedFreeDate;
+        //private bool _isFreeDateSelected;
+
+        private readonly AccommodationService _accommodationService;
+        public ICollectionView AccommodationsSourceCollection => _accommodationsCollection.View;
+        public AccommodationStatisticsViewModel()
+        {
+            _accommodationService = new AccommodationService();
+
+            _accommodationsCollection = new CollectionViewSource
+            {
+                Source = _accommodationService.GetAccommodationNames()
+            };
+        }
+
+        /*public bool IsFreeDateSelected
+        {
+            get => _isFreeDateSelected;
+            set
+            {
+                _isFreeDateSelected = value;
+                OnPropertyChanged();
+            }
+        }
+        public FreeDatesDTO? SelectedFreeDate
+        {
+            get => _selectedFreeDate;
+
+            set
+            {
+                _selectedFreeDate = value;
+                IsFreeDateSelected = true;
+                OnPropertyChanged();
+            }
+        }*/
+
+    }
+}
