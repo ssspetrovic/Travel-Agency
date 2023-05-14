0c95a3bd6c6bfb842b9e13b81307ff2906568719
agajic <andrejgajic777@gmail.com>
2023-05-14 16:44:11 +0200

    created Renovation OverView

---
+++ TravelAgency/TravelAgency/Service/RenovationService.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TravelAgency.Model;
+using TravelAgency.Repository;
+
+namespace TravelAgency.Service
+{
+    public class RenovationService
+    {
+        public ObservableCollection<Renovation> GetPreviousRenovations()
+        {
+            var renovationRepository = new RenovationRepository();
+            ObservableCollection<Renovation> previousRenovations = new ObservableCollection<Renovation>();
+
+            ObservableCollection<Renovation> temp = renovationRepository.GetAll();
+            foreach(Renovation renovation in temp)
+            {
+                if(renovation.StartDate <= DateTime.Now)
+                {
+                    previousRenovations.Add(renovation);
+                }
+            }
+            return previousRenovations;
+        }
+
+        public ObservableCollection<Renovation> GetFutureRenovations()
+        {
+            var renovationRepository = new RenovationRepository();
+            ObservableCollection<Renovation> futureRenovations = new ObservableCollection<Renovation>();
+
+            ObservableCollection<Renovation> temp = renovationRepository.GetAll();
+            foreach (Renovation renovation in temp)
+            {
+                if (renovation.StartDate <= DateTime.Now)
+                {
+                    futureRenovations.Add(renovation);
+                }
+            }
+            return futureRenovations;
+        }
+    }
+}
---
+++ TravelAgency/TravelAgency/ViewModel/RenovationOverviewViewModel.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using TravelAgency.Service;
+
+namespace TravelAgency.ViewModel
+{
+    public class RenovationOverviewViewModel : BaseViewModel
+    {
+        private readonly CollectionViewSource _previousRenovationsCollection;
+        private readonly CollectionViewSource _futureRenovationsCollection;
+        public new event PropertyChangedEventHandler? PropertyChanged;
+
+        public ICollectionView PreviousRenovationsSourceCollection => _previousRenovationsCollection.View;
+        public ICollectionView FutureRenovationsSourceCollection => _futureRenovationsCollection.View;
+
+        private readonly RenovationService _renovationService;
+        public RenovationOverviewViewModel()
+        {
+            _renovationService = new RenovationService();
+
+            _previousRenovationsCollection = new CollectionViewSource
+            {
+                Source = _renovationService.GetPreviousRenovations()
+            };
+
+            _futureRenovationsCollection = new CollectionViewSource
+            {
+                Source = _renovationService.GetFutureRenovations()
+            };
+        }
+    }
+}
