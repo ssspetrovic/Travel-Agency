12ccadf18399a1c7b7eceb23bb91b9012dbde9b3
Srdjan Petrovic <s.petrovic852@gmail.com>
2023-05-15 12:40:27 +0200

    Remove unused imports

--- TravelAgency/TravelAgency/View/Tourist/NotificationsView.xaml.cs
+++ TravelAgency/TravelAgency/View/Tourist/NotificationsView.xaml.cs
@@ -1,24 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-
-namespace TravelAgency.View.Tourist
+﻿namespace TravelAgency.View.Tourist
 {
     /// <summary>
     /// Interaction logic for NotificationsView.xaml
     /// </summary>
-    public partial class NotificationsView : Page
+    public partial class NotificationsView
     {
         public NotificationsView()
         {
