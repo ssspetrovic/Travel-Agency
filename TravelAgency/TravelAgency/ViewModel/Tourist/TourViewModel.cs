e50f95e848ed8807ea07027181fa7a68e255df33
Srdjan Petrovic <s.petrovic852@gmail.com>
2023-05-17 17:13:49 +0200

    Bind image click to a command

--- TravelAgency/TravelAgency/ViewModel/Tourist/TourViewModel.cs
+++ TravelAgency/TravelAgency/ViewModel/Tourist/TourViewModel.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Windows.Controls;
+using TravelAgency.Command;
 using TravelAgency.Model;
 using TravelAgency.Service;
 
@@ -11,6 +13,8 @@ namespace TravelAgency.ViewModel.Tourist
         public string StartingDate { get; set; }
         public string KeyPoints { get; set; }
         public List<Uri> ParsedUris { get; set; }
+        
+        public RelayCommand OpenImageCommand { get; set; }
 
         public TourViewModel(Tour tour)
         {
@@ -20,6 +24,18 @@ namespace TravelAgency.ViewModel.Tourist
             StartingDate = Tour.Date.Split(", ")[0];
             KeyPoints = tourService.GetFormattedKeyPoints(Tour.KeyPoints);
             ParsedUris = tourService.ParsePhotosUris(Tour.Photos);
+        }
+
+        private void Execute_OpenImageCommand(object parameter)
+        {
+            if (parameter is not Image image) return;
+            
+
+        }
+
+        private bool CanExecute_OpenImageCommand(object parameter)
+        {
+            return parameter is Image;
         }
     }
 }
