afebfa9ad6343ec045f76820d1af98656931a603
Srdjan Petrovic <s.petrovic852@gmail.com>
2023-05-17 12:46:32 +0200

    Add view class

---
+++ TravelAgency/TravelAgency/View/Tourist/TourView.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace TravelAgency.View.Tourist
+{
+    /// <summary>
+    /// Interaction logic for TourView.xaml
+    /// </summary>
+    public partial class TourView
+    {
+        public TourView()
+        {
+            InitializeComponent();
+        }
+    }
+}
