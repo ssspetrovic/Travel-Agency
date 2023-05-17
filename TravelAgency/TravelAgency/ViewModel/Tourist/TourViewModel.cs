25bdc5f8253a5f29c5e606085368c6b47b5d1bcc
Srdjan Petrovic <s.petrovic852@gmail.com>
2023-05-17 16:58:15 +0200

    Increase font size

--- TravelAgency/TravelAgency/ViewModel/Tourist/TourViewModel.cs
+++ TravelAgency/TravelAgency/ViewModel/Tourist/TourViewModel.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using TravelAgency.Model;
 using TravelAgency.Service;
 
@@ -9,10 +8,8 @@ namespace TravelAgency.ViewModel.Tourist
     internal class TourViewModel
     {
         public Tour Tour { get; set; }
+        public string StartingDate { get; set; }
         public string KeyPoints { get; set; }
-        //public Uri FirstImageSourceUri { get; set; }
-        //public Uri SecondImageSourceUri { get; set; }
-        //public Uri ThirdImageSourceUri { get; set; }
         public List<Uri> ParsedUris { get; set; }
 
         public TourViewModel(Tour tour)
@@ -20,12 +17,9 @@ namespace TravelAgency.ViewModel.Tourist
             Tour = tour;
             var tourService = new TourService();
 
+            StartingDate = Tour.Date.Split(", ")[0];
             KeyPoints = tourService.GetFormattedKeyPoints(Tour.KeyPoints);
             ParsedUris = tourService.ParsePhotosUris(Tour.Photos);
-
-            //FirstImageSourceUri = parsedUris.Count > 0 ? parsedUris[0] : new Uri("");
-            //SecondImageSourceUri = parsedUris.Count > 1 ? parsedUris[0] : FirstImageSourceUri;
-            //ThirdImageSourceUri = parsedUris.Count > 2 ? parsedUris[0] : FirstImageSourceUri;
         }
     }
 }
