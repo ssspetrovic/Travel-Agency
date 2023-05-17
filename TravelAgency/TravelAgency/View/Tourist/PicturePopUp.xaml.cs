e6ff799be33aef7ba8f5b5ba5b4bd04fc14d0f71
Srdjan Petrovic <s.petrovic852@gmail.com>
2023-05-17 13:44:27 +0200

    Add picture view UC

---
+++ TravelAgency/TravelAgency/View/Tourist/PicturePopUp.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace TravelAgency.View.Tourist
+{
+    /// <summary>
+    /// Interaction logic for PicturePopUp.xaml
+    /// </summary>
+    public partial class PicturePopUp
+    {
+        public PicturePopUp()
+        {
+            InitializeComponent();
+        }
+    }
+}
