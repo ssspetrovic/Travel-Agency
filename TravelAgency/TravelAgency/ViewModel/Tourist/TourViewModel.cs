4081d9b331fe02bbcd73d1d22d64b4741aa88dec
Srdjan Petrovic <s.petrovic852@gmail.com>
2023-05-17 17:59:43 +0200

    Complete tour view

--- TravelAgency/TravelAgency/ViewModel/Tourist/TourViewModel.cs
+++ TravelAgency/TravelAgency/ViewModel/Tourist/TourViewModel.cs
@@ -1,9 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Windows.Controls;
+using System.Windows;
 using TravelAgency.Command;
 using TravelAgency.Model;
 using TravelAgency.Service;
+using TravelAgency.View.Tourist;
 
 namespace TravelAgency.ViewModel.Tourist
 {
@@ -24,18 +25,25 @@ namespace TravelAgency.ViewModel.Tourist
             StartingDate = Tour.Date.Split(", ")[0];
             KeyPoints = tourService.GetFormattedKeyPoints(Tour.KeyPoints);
             ParsedUris = tourService.ParsePhotosUris(Tour.Photos);
+
+            OpenImageCommand = new RelayCommand(Execute_OpenImageCommand, CanExecute_OpenImageCommand);
         }
 
         private void Execute_OpenImageCommand(object parameter)
         {
-            if (parameter is not Image image) return;
-            
+            if (parameter is not Uri imageUri) return;
 
+            var dialog = new PicturePopUpDialog(imageUri)
+            {
+                Owner = Application.Current.MainWindow
+            };
+
+            dialog.Show();
         }
 
         private bool CanExecute_OpenImageCommand(object parameter)
         {
-            return parameter is Image;
+            return parameter is Uri;
         }
     }
 }
