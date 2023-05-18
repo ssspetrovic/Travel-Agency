1187e2dc1152f9fad63a3e4e92d0c4aeb1679e1e
Srdjan Petrovic <s.petrovic852@gmail.com>
2023-05-18 17:48:23 +0200

    Add wizard view

---
+++ TravelAgency/TravelAgency/View/Tourist/WizardDialogView.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace TravelAgency.View.Tourist
+{
+    /// <summary>
+    /// Interaction logic for WizardDialogView.xaml
+    /// </summary>
+    public partial class WizardDialogView
+    {
+        public WizardDialogView()
+        {
+            InitializeComponent();
+        }
+    }
+}
