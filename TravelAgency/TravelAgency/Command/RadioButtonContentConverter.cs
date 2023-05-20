0de0d1181034d7f3a39cd22c74130aac3d23d641
agajic <andrejgajic777@gmail.com>
2023-05-20 02:30:07 +0200

    small update

---
+++ TravelAgency/TravelAgency/Command/RadioButtonContentConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace TravelAgency.Command
+{
+    public class RadioButtonContentConverter : IValueConverter
+    {           
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+                var radioButtonContent = (string)parameter;
+                var isChecked = (bool)value;
+
+                return isChecked ? radioButtonContent : null;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- TravelAgency/TravelAgency/ViewModel/GradeGuestViewModel.cs
+++ TravelAgency/TravelAgency/ViewModel/GradeGuestViewModel.cs
@@ -1,10 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 using System.Windows.Data;
+using TravelAgency.Command;
 using TravelAgency.Model;
 using TravelAgency.Repository;
 
@@ -16,11 +19,19 @@ namespace TravelAgency.ViewModel
         public new event PropertyChangedEventHandler? PropertyChanged;
         private Reservation? _selectedReservation;
         private bool _isReservatoinSelected;
+        public RelayCommand BtnGradeGuest { get; set; }
+
+        private bool _radioButtonValue1;
+        private bool _radioButtonValue2;
+        private object _selectedOption1;
+        private object _selectedOption2;
 
         private readonly ReservationRepository _reservationRepository;
         public ICollectionView ReservationsSourceCollection => _reservationsCollection.View;
         public GradeGuestViewModel()
         {
+            BtnGradeGuest = new RelayCommand(Execute_BtnGradeGuest);
+
             _reservationRepository = new ReservationRepository();
 
             _reservationsCollection = new CollectionViewSource
@@ -28,6 +39,11 @@ namespace TravelAgency.ViewModel
                 Source = _reservationRepository.GetReservationsToGrade()
             };
             _reservationRepository = new ReservationRepository();
+        }
+
+        private void Execute_BtnGradeGuest(object parameter)
+        {
+            MessageBox.Show(SelectedOption1.ToString());
         }
 
         public bool IsReservationSelected
@@ -50,5 +66,49 @@ namespace TravelAgency.ViewModel
                 OnPropertyChanged();
             }
         }
+
+        public bool RadioButtonValue1
+        {
+            get => _radioButtonValue1;
+
+            set
+            {
+                _radioButtonValue1 = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public bool RadioButtonValue2
+        {
+            get => _radioButtonValue2;
+
+            set
+            {
+                _radioButtonValue2 = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public object SelectedOption1
+        {
+            get => _selectedOption1;
+
+            set
+            {
+                _selectedOption1 = value;
+                OnPropertyChanged();
+            }
+        }
+        public object SelectedOption2
+        {
+            get => _selectedOption2;
+
+            set
+            {
+                _selectedOption2 = value;
+                OnPropertyChanged();
+            }
+        }
+
     }
 }
