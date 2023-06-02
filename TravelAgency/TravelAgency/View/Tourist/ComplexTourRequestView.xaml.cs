06b214179e05c6e7916c78d7bdc9ac72689b5d53
Srdjan Petrovic <s.petrovic852@gmail.com>
2023-06-02 11:45:39 +0200

    Create view

---
+++ TravelAgency/TravelAgency/View/Tourist/ComplexTourRequestView.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace TravelAgency.View.Tourist
+{
+    /// <summary>
+    /// Interaction logic for ComplexTourRequestView.xaml
+    /// </summary>
+    public partial class ComplexTourRequestView
+    {
+        public ComplexTourRequestView()
+        {
+            InitializeComponent();
+        }
+    }
+}
