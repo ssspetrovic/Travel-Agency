6282b84e46a905bab4fd1251ef082f9cfc6c3c18
Srdjan Petrovic <s.petrovic852@gmail.com>
2023-06-03 16:56:40 +0200

    Bind commands to view model

--- TravelAgency/TravelAgency/ViewModel/Tourist/ComplexTourRequestViewModel.cs
+++ TravelAgency/TravelAgency/ViewModel/Tourist/ComplexTourRequestViewModel.cs
@@ -9,17 +9,43 @@ namespace TravelAgency.ViewModel.Tourist
         private readonly TouristViewModel _touristViewModel;
 
         public RelayCommand CancelCommand { get; set; }
+        public RelayCommand SubmitCommand { get; set; }
+        public RelayCommand AddTourPartCommand { get; set; }
 
         public ComplexTourRequestViewModel(NavigationService navigationService, TouristViewModel touristViewModel)
         {
             _navigationService = navigationService;
             _touristViewModel = touristViewModel;
             CancelCommand = new RelayCommand(Execute_CancelCommand);
+            SubmitCommand = new RelayCommand(Execute_SubmitCommand, CanExecute_SubmitCommand);
+            AddTourPartCommand = new RelayCommand(Execute_AddTourPartCommand, CanExecute_AddTourPartCommand);
         }
 
         private void Execute_CancelCommand(object parameter)
         {
             _navigationService.GoBack();
+        }
+
+        private void Execute_SubmitCommand(object parameter)
+        {
+            // TODO Implement
+        }
+
+        private bool CanExecute_SubmitCommand(object parameter)
+        {
+            // TODO Implement
+            return false;
+        }
+
+        private void Execute_AddTourPartCommand(object parameter)
+        {
+            // TODO Implement
+        }
+
+        private bool CanExecute_AddTourPartCommand(object parameter)
+        {
+            // TODO Implement
+            return false;
         }
     }
 }
