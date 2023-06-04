8c9fd6b74ee04c07bd310d57c5c3c284b09f1f75
Nikwiza <nikola.kerleta1@gmail.com>
2023-06-04 10:32:19 -0700

    Adding forum and content models

---
+++ TravelAgency/TravelAgency/Model/Forum.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TravelAgency.Model
+{
+    public class Forum
+    {
+        public int Id { get; set; }
+        public int GuestId { get; set; }
+        public bool IsClosed { get; set; } = false;
+        public int GuestCommentNumber { get; set; } = 0;
+        public int OwnerCommentNumber { get; set; } = 0;
+
+        public Forum() { }
+
+        public Forum(int guestId, bool isClosed, int guestCommentNumber, int ownerCommentNumber)
+        {
+            GuestId = guestId;
+            IsClosed = isClosed;
+            GuestCommentNumber = guestCommentNumber;
+            OwnerCommentNumber = ownerCommentNumber;
+        }
+    }
+}
