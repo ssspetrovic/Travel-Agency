68500a02e41d924979cb94abfa7ca0246a22b5df
Nikwiza <nikola.kerleta1@gmail.com>
2023-06-05 07:48:39 -0700

    Starting the home window preperation

---
+++ TravelAgency/TravelAgency/ViewModel/GuestViewModel.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Windows.Navigation;
+using TravelAgency.Command;
+using TravelAgency.Model;
+using TravelAgency.Service;
+using TravelAgency.View;
+using TravelAgency.View.Tourist;
+using TravelAgency.WindowHelpers;
+
+namespace TravelAgency.ViewModel
+{
+    public class GuestViewModel: BaseViewModel
+    {
+        private readonly IWindowManager _windowManager;
+        private readonly NavigationService _navigationService;
+        public RelayCommand NavigateToHomePageCommand { get; set; }
+        public RelayCommand NavigateToMyToursPageCommand { get; set; }
+        public RelayCommand NavigateToMyVouchersPageCommand { get; set; }
+        public RelayCommand NavigateToMyRequestsCommand { get; set; }
+        public RelayCommand NavigateToTourReservationPageCommand { get; set; }
+        public RelayCommand NavigateToUserProfileCommand { get; set; }
+        public RelayCommand NavigateToRequestTourPageCommand { get; set; }
+        public RelayCommand NavigateToNotificationsPageCommand { get; set; }
+        public RelayCommand CloseWindowCommand { get; set; }
+        public RelayCommand SignOutCommand { get; set; }
+
+        public GuestViewModel(NavigationService navigationService)
+        {
+            _windowManager = new WindowManager();
+            _navigationService = navigationService;
+        }
+
+    }
+}
